eb89bd1d3f5d93affa9fe9a4a36056cd77e4394f
yoonhee kim <mini.yoonhee@gmail.com>
2019-04-01 20:31:38 -0400

    Updates

---
+++ precache-manifest.2176fbf85df81a22a3cf66d0b8475ca7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  },
+  {
+    "revision": "445629e1d23810cfe276",
+    "url": "/clicky/static/js/main.1cb9d633.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "445629e1d23810cfe276",
+    "url": "/clicky/static/css/main.83b29187.chunk.css"
+  },
+  {
+    "revision": "21838f34a51fad56c6dde99c91a5a987",
+    "url": "/clicky/index.html"
+  }
+];
